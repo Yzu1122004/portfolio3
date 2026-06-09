99f3fda3e7e3da5e842c013b931bec5780c2dae5
OWO-OUO <tsaiyusan2@gmail.com>
2026-06-09 21:55:21 +0800

    Fix InfinityFree database name

--- php/db_connect.php
+++ php/db_connect.php
@@ -3,7 +3,7 @@
 $DB_HOST = 'sql209.infinityfree.com';
 $DB_USER = 'if0_42139419';
 $DB_PASS = 'OWOOHO111222333';
-$DB_NAME = 'if0_42139419_XXX';
+$DB_NAME = 'if0_42139419_test';
 $DB_PORT = 3306;
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
